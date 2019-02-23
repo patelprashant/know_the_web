d400f533f4131331cde746e14a37a61a47031a7d
Prashant <prashant.patel77@gmail.com>
2019-02-23 16:43:07 +1100

    Added data/json file and parsed it to the view.

--- lib/statusCodeList/model/statusCode.dart
+++ lib/statusCodeList/model/statusCode.dart
@@ -5,10 +5,20 @@ class StatusCodeModel {
   final String spec_title;
   final String spec_href;
 
-  const StatusCodeModel(
+  StatusCodeModel(
       {this.code,
       this.phrase,
       this.description,
       this.spec_title,
       this.spec_href});
+
+  factory StatusCodeModel.fromJson(Map<String, dynamic> json) {
+    return new StatusCodeModel(
+      code: json['code'] as String,
+      phrase: json['phrase'] as String,
+      description: json['description'] as String,
+      spec_title: json['spec_title'] as String,
+      spec_href: json['spec_href'] as String,
+    );
+  }
 }
--- lib/statusCodeList/status_codes_page.dart
+++ lib/statusCodeList/status_codes_page.dart
@@ -1,57 +1,87 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:know_the_web/statusCodeList/model/statusCode.dart';
 import 'package:know_the_web/statusCodeList/status_codes.dart';
 
-class StatusCodesPage extends StatelessWidget {
+class StatusCodesPage extends StatefulWidget {
+  @override
+  StatusCodePageState createState() => new StatusCodePageState();
+}
+
+class StatusCodePageState extends State<StatusCodesPage> {
+  List<StatusCodeModel> statusCodeList;
+
   _buildStatusCodeList() {
     return <StatusCodeModel>[
-      const StatusCodeModel(
+      StatusCodeModel(
           code: '101',
           phrase: 'Phares for 101',
           description:
               'This is long long long very long description, This is long long long very long description ,This is long long long very long description'),
-      const StatusCodeModel(
+      StatusCodeModel(
           code: '102',
           phrase: 'Phares for 102',
           description: 'This is long long long very long description'),
-      const StatusCodeModel(
+      StatusCodeModel(
           code: '103',
           phrase: 'Phares for 103',
           description: 'This is long long long very long description'),
-      const StatusCodeModel(
+      StatusCodeModel(
           code: '104',
           phrase: 'Phares for 104',
           description: 'This is long long long very long description'),
-      const StatusCodeModel(
+      StatusCodeModel(
           code: '105',
           phrase: 'Phares for 105',
           description: 'This is long long long very long description'),
-      const StatusCodeModel(
+      StatusCodeModel(
           code: '106',
           phrase: 'Phares for 106',
           description: 'This is long long long very long description'),
-      const StatusCodeModel(
+      StatusCodeModel(
           code: '107',
           phrase: 'Phares for 107',
           description: 'This is long long long very long description'),
-      const StatusCodeModel(
+      StatusCodeModel(
           code: '108',
           phrase: 'Phares for 108',
           description: 'This is long long long very long description'),
-      const StatusCodeModel(
+      StatusCodeModel(
           code: '109',
           phrase: 'Phares for 109',
           description: 'This is long long long very long description'),
-      const StatusCodeModel(
+      StatusCodeModel(
           code: '110',
           phrase: 'Phares for 110',
           description: 'This is long long long very long description'),
     ];
   }
 
-//new StatusCodes(_buildStatusCodeList());
+//new StatusCodes(_buildStatusCodeList())
   @override
   Widget build(BuildContext context) {
-    return new Expanded(child: new StatusCodes(_buildStatusCodeList()));
+    return new Expanded(
+        child: new FutureBuilder(
+            future: DefaultAssetBundle.of(context)
+                .loadString('assets/data/status-codes.json'),
+            builder: (context, snapshot) {
+              statusCodeList = parseJson(snapshot.data.toString());
+              return statusCodeList.isNotEmpty
+                  ? new StatusCodes(statusCodeList)
+                  : new Center(
+                      child: new CircularProgressIndicator(),
+                    );
+            }));
   }
+}
+
+List<StatusCodeModel> parseJson(String response) {
+  if (response == null) {
+    return [];
+  }
+  final parsed = json.decode(response.toString()).cast<Map<String, dynamic>>();
+  return parsed
+      .map<StatusCodeModel>((json) => new StatusCodeModel.fromJson(json))
+      .toList();
 }
